41fdaaa3aa718d911c7d1165758102c013f82230
niceoneallround <niceoneallround@gmail.com>
2015-10-16 17:00:24 -0700

    Add serverConfig so can share with connectors

---
+++ serverConfig/lib/serverConfig.js
@@ -0,0 +1,212 @@
+/*jslint node: true, vars: true */
+
+//
+//
+// A config that wraps
+// HOSTNAME
+// x_PORT
+// x_PROTOCOL
+// x_PORT_INSIDE_DOCKER
+// MODE
+// LOG_ENTRIES
+// VERSION
+//
+var assert = require('assert'),
+    LOG_P = require('../../logger/lib/logger').PROPERTIES,
+    util = require('util');
+
+//
+// Get the configuration for this service
+// *id the service id
+// *type the service type
+//
+function create() {
+  'use strict';
+  var c = {};
+
+  c.MODE = process.env.MODE;
+
+  //
+  // If no mode default to production
+  //
+  if (!c.MODE) {
+    c.MODE = 'PROD';
+  }
+
+  if (isProduction(c)) {
+    // network config
+    c.HOSTNAME = process.env.PROD_HOSTNAME;
+    c.PROD_PORT = process.env.PROD_PORT;
+    c.PROD_PROTOCOL = process.env.PROD_PROTOCOL;
+    c.PROD_PORT_INSIDE_DOCKER = process.env.PROD_PORT_INSIDE_DOCKER;
+    c.HOST_TYPE = process.env.PROD_HOST_TYPE;
+  } else if (isDevelopment(c)) {
+    // network config
+    c.HOSTNAME = process.env.DEV_HOSTNAME;
+    c.DEV_PORT = process.env.DEV_PORT;
+    c.DEV_PROTOCOL = process.env.DEV_PROTOCOL;
+    c.DEV_PORT_INSIDE_DOCKER = process.env.DEV_PORT_INSIDE_DOCKER;
+    c.HOST_TYPE = process.env.DEV_HOST_TYPE;
+  }
+
+  // by default do not use unless an environmental to override.
+  c[LOG_P.useLogEntries] = false;
+  if (process.env.LOG_ENTRIES) {
+    c[LOG_P.useLogEntries] = process.env.LOG_ENTRIES;
+  }
+
+  c.VERSION_NUMBER = 'not-set';
+  if (process.env.VERSION_NUMBER) {
+    c.VERSION_NUMBER = process.env.VERSION_NUMBER;
+  }
+
+  c.getHost = function() {
+    return getHost(c);
+  };
+
+  c.getHostname = function() {
+    return getHostname(c);
+  };
+
+  c.getHostnameWithPort = function() {
+    return getHostnameWithPort(c);
+  };
+
+  c.getPort = function() {
+    return getPort(c);
+  };
+
+  c.getPortInsideDocker = function() {
+    return getPortInsideDocker(c);
+  };
+
+  c.getProtocol = function() {
+    return getProtocol(c);
+  };
+
+  c.isDevelopment = function() {
+    return isDevelopment(c);
+  };
+
+  c.isProduction = function() {
+    return isProduction(c);
+  };
+
+  return c;
+}
+
+function isDevelopment(c) {
+  'use strict';
+  assert(c.MODE, util.format('No MODE in config:%j', c));
+  return (c.MODE === 'DEV');
+}
+
+function isProduction(c) {
+  'use strict';
+  assert(c.MODE, util.format('No MODE in config:%j', c));
+  return (c.MODE === 'PROD');
+}
+
+//
+// depending on mode return hostname
+//
+function getHostname(c) {
+  'use strict';
+
+  if (c.HOSTNAME) {
+    return c.HOSTNAME;
+  } else {
+    return 'localhost';
+  }
+}
+
+//
+// Depending on mode return the port
+//
+function getPortInsideDocker(c) {
+  'use strict';
+  var portInsideDocker = 8080;
+
+  if (isProduction(c) && c.PROD_PORT_INSIDE_DOCKER) {
+    portInsideDocker = c.PROD_PORT_INSIDE_DOCKER;
+  } else if (isDevelopment(c) && c.DEV_PORT_INSIDE_DOCKER) {
+    portInsideDocker = c.DEV_PORT_INSIDE_DOCKER;
+  }
+
+  return portInsideDocker;
+}
+
+//
+// Depending on mode return the port
+//
+function getPort(c) {
+  'use strict';
+  var port = 8080;
+
+  if (isProduction(c) && c.PROD_PORT) {
+    port = c.PROD_PORT;
+  } else if (isDevelopment(c) && c.DEV_PORT) {
+    port = c.DEV_PORT;
+  }
+
+  return port;
+}
+
+//
+// Depending on mode return the protocol
+//
+function getProtocol(c) {
+  'use strict';
+  var protocol = 'http';
+
+  if (isProduction(c) && c.PROD_PROTOCOL) {
+    protocol = c.PROD_PROTOCOL;
+  } else if (isDevelopment(c) && c.DEV_PROTOCOL) {
+    protocol = c.DEV_PROTOCOL;
+  }
+
+  return protocol;
+}
+
+//
+// Depending on mode return the combination of hostname and port
+//
+function getHostnameWithPort(c) {
+  'use strict';
+  var result = 'localhost:8080';
+
+  if (isProduction(c)) {
+    result = getHostname(c);
+    if (c.PROD_PORT) {
+      result = result + ':' + c.PROD_PORT;
+    }
+  } else if (isDevelopment(c)) {
+    result = getHostname(c);
+    if (c.DEV_PORT) {
+      result = result + ':' + c.DEV_PORT;
+    }
+  }
+
+  return result;
+}
+
+//
+// returns combination of protocol, hostname and port
+//
+function getHost(c) {
+  'use strict';
+  return getProtocol(c) + '://' + getHostnameWithPort(c);
+}
+
+module.exports = {
+  create: create
+  /*getHost: getHost,
+  getHostname: getHostname,
+  getHostnameWithPort: getHostnameWithPort,
+  getPort: getPort,
+  getPortInsideDocker: getPortInsideDocker,
+  getProtocol: getProtocol,
+  getServiceConfig: getServiceConfig,
+  isDevelopment: isDevelopment,
+  isProduction: isProduction*/
+};
---
+++ serverConfig/test/testServerConfig.js
@@ -0,0 +1,231 @@
+/*jslint node: true, vars: true */
+
+var assert = require('assert'),
+    configUtils = require('../lib/serverConfig'),
+    LOG_P = require('../../logger/lib/logger').PROPERTIES,
+    util = require('util');
+
+describe('Server Config Utils Tests', function() {
+  'use strict';
+
+  it('0.1 it should return a configuration', function() {
+
+    var c = configUtils.create();
+    assert(c, 'No config returned from create');
+  });
+
+  describe('1 test production versus dev flags', function() {
+    it('1.1 it should support isProduction', function() {
+
+      var c = configUtils.create();
+
+      c.MODE = 'PROD';
+      c.isProduction().should.be.equal(true);
+
+      c.MODE = 'DEV';
+      c.isProduction().should.be.equal(false);
+    });
+
+    it('1.2 it should support isDevelopment', function() {
+
+      var c = configUtils.create();
+
+      c.MODE = 'DEV';
+      c.isDevelopment().should.be.equal(true);
+
+      c.MODE = 'PROD';
+      c.isDevelopment().should.be.equal(false);
+    });
+  });
+
+  describe('2 test getHostnameWithPort', function() {
+    it('2.1 it should work with PROD and hostname', function() {
+
+      var c = configUtils.create();
+
+      c.MODE = 'PROD';
+      c.HOSTNAME = 'hell_yea';
+      c.getHostnameWithPort().should.be.equal('hell_yea');
+
+      c.PROD_PORT = '666';
+      c.getHostnameWithPort().should.be.equal('hell_yea:666');
+
+    });
+
+    it('2.2 it should work with DEV and hostname', function() {
+
+      var c = configUtils.create();
+
+      c.MODE = 'DEV';
+      c.HOSTNAME = 'hell_no';
+      c.getHostnameWithPort().should.be.equal('hell_no');
+
+      c.DEV_PORT = '999';
+      c.getHostnameWithPort().should.be.equal('hell_no:999');
+
+    });
+
+    it('2.2 it should work with no hostname', function() {
+
+      var c = configUtils.create();
+
+      c.MODE = 'DEV';
+      c.getHostnameWithPort().should.be.equal('localhost');
+
+      c.DEV_PORT = '999';
+      c.getHostnameWithPort().should.be.equal('localhost:999');
+
+    });
+  });
+
+  describe('3 test getHostname', function() {
+    it('it should work with PROD', function() {
+
+      var c = configUtils.create();
+
+      c.MODE = 'PROD';
+      c.HOSTNAME = 'hell_yea';
+      c.getHostname().should.be.equal('hell_yea');
+    });
+
+    it('3.1 it should work with DEV', function() {
+
+      var c = configUtils.create();
+
+      c.MODE = 'DEV';
+      c.HOSTNAME = 'hell_no';
+      c.getHostname().should.be.equal('hell_no');
+
+    });
+
+    it('3.2 it should work with no hostname', function() {
+
+      var c = configUtils.create();
+
+      c.MODE = 'DEV';
+      c.getHostnameWithPort().should.be.equal('localhost');
+    });
+  });
+
+  describe('4 test getPort', function() {
+    it('it should work with PROD and port', function() {
+
+      var c = configUtils.create();
+
+      c.MODE = 'PROD';
+      c.PROD_PORT = '666';
+      c.getPort().should.be.equal('666');
+    });
+
+    it('4.1 it should work with DEV and hostname', function() {
+
+      var c = configUtils.create();
+
+      c.MODE = 'DEV';
+      c.DEV_PORT = '999';
+      c.getPort().should.be.equal('999');
+    });
+  });
+
+  describe('5 test getPortInsideDocker', function() {
+    it('5.1 it should work with PROD and port', function() {
+
+      var c = configUtils.create();
+
+      c.MODE = 'PROD';
+      c.PROD_PORT_INSIDE_DOCKER = '666';
+      c.getPortInsideDocker().should.be.equal('666');
+    });
+
+    it('5.2 it should work with DEV and hostname', function() {
+
+      var c = configUtils.create();
+
+      c.MODE = 'DEV';
+      c.DEV_PORT_INSIDE_DOCKER = '999';
+      c.getPortInsideDocker().should.be.equal('999');
+    });
+  });
+
+  describe('6 test getProtocol', function() {
+    it('6.1 it should work with PROD and protocol', function() {
+
+      var c = configUtils.create();
+
+      c.MODE = 'PROD';
+      c.PROD_PROTOCOL = 'http_1';
+      c.getProtocol().should.be.equal('http_1');
+    });
+
+    it('6.2 it should work with DEV and protocol', function() {
+
+      var c = configUtils.create();
+
+      c.MODE = 'DEV';
+      c.DEV_PROTOCOL = 'http_2';
+      c.getProtocol().should.be.equal('http_2');
+    });
+
+    it('6.3 it should default to http if dev mode', function() {
+
+      var c = configUtils.create();
+
+      c.MODE = 'DEV';
+      c.getProtocol().should.be.equal('http');
+    });
+
+    it('6.4 it should default to http if prod mode', function() {
+
+      var c = configUtils.create();
+
+      c.MODE = 'PROD';
+      c.getProtocol().should.be.equal('http');
+    });
+  });
+
+  describe('7 test log entries', function() {
+
+    it('7.1 it should be false by default', function() {
+      var c = configUtils.create();
+      assert(!c[LOG_P.useLogEntries], util.format('Log entries by default should be false:%j', c));
+    });
+
+    it('7.2 it should pick up from env ', function() {
+      var c;
+
+      process.env.LOG_ENTRIES = true;
+      c = configUtils.create();
+      assert(c[LOG_P.useLogEntries], util.format('Log entries should be true:%j', c));
+    });
+  });
+
+  describe('8 test VERSION_NUMBER', function() {
+
+    it('8.1 it should be false by default', function() {
+      var c = configUtils.create();
+      assert((c.VERSION_NUMBER === 'not-set'), util.format('Version number not set to not set :):%j', c));
+    });
+
+    it('8.2 it should pick up from env ', function() {
+      var c;
+
+      process.env.VERSION_NUMBER = '1.0.23';
+      c = configUtils.create();
+      assert((c.VERSION_NUMBER === '1.0.23'), util.format('Version number not set to not set :):%j', c));
+    });
+  });
+
+  describe('9 test getHost', function() {
+
+    it('9.1 it should work with PROD', function() {
+
+      var c = configUtils.create();
+      c.MODE = 'PROD';
+      c.HOSTNAME = 'hell_yea';
+      c.PROD_PORT = '23';
+      c.PROD_PROTOCOL = 'http';
+      c.getHost().should.be.equal('http://hell_yea:23');
+    });
+  });
+
+});
