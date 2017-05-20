3afce7d7432fa96598d199939980e6c718f672d8
niceoneallround <niceoneallround@gmail.com>
2017-05-20 13:01:11 -0700

    Not mandatory to have JWT

--- serverConfig/lib/configFileVerifier.js
+++ serverConfig/lib/configFileVerifier.js
@@ -17,28 +17,30 @@ function verify(config) {
     assert(config.terminate_tls.private_key_file, util.format('No config.terminate_tls.private_key_file property cannot configure:%j', config));
   }
 
-  // Ensure the jwt configuration
-  assert(config.jwt, util.format('No config.jwt property cannot configure signing and verifying JWTs:%j', config));
-  assert(config.jwt.signer, util.format('No config.jwt.signer property cannot configure signing JWTs:%j', config));
+  if (config.jwt) {
+    // Ensure the jwt configuration
+    assert(config.jwt, util.format('No config.jwt property cannot configure signing and verifying JWTs:%j', config));
+    assert(config.jwt.signer, util.format('No config.jwt.signer property cannot configure signing JWTs:%j', config));
 
-  let signer = config.jwt.signer;
-  switch (signer.alg) {
+    let signer = config.jwt.signer;
+    switch (signer.alg) {
 
-    case 'HS256': {
-      assert(signer.HS256, util.format('No config.jwt.signer.HS256 property cannot configure signing JWTs:%j', config));
-      break;
-    }
+      case 'HS256': {
+        assert(signer.HS256, util.format('No config.jwt.signer.HS256 property cannot configure signing JWTs:%j', config));
+        break;
+      }
 
-    case 'RS256': {
-      assert(signer.RS256, util.format('No config.jwt.signer.RS256 property cannot configure signing JWTs:%j', config));
-      assert(signer.RS256.certificate_file, util.format('No config.jwt.signer.RS256.certificate_file property cannot configure signing JWTs:%j', config));
-      assert(signer.RS256.public_key_file, util.format('No config.jwt.signer.RS256.public_key_file property cannot configure signing JWTs:%j', config));
-      assert(signer.RS256.private_key_file, util.format('No config.jwt.signer.RS256.private_key_file property cannot configure signing JWTs:%j', config));
-      break;
-    }
+      case 'RS256': {
+        assert(signer.RS256, util.format('No config.jwt.signer.RS256 property cannot configure signing JWTs:%j', config));
+        assert(signer.RS256.certificate_file, util.format('No config.jwt.signer.RS256.certificate_file property cannot configure signing JWTs:%j', config));
+        assert(signer.RS256.public_key_file, util.format('No config.jwt.signer.RS256.public_key_file property cannot configure signing JWTs:%j', config));
+        assert(signer.RS256.private_key_file, util.format('No config.jwt.signer.RS256.private_key_file property cannot configure signing JWTs:%j', config));
+        break;
+      }
 
-    default: {
-      assert(false, util.format('unknown jwt signing type in config.jwt.signer.alg:%j', config));
+      default: {
+        assert(false, util.format('unknown jwt signing type in config.jwt.signer.alg:%j', config));
+      }
     }
   }
 }
